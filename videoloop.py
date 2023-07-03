b18e8c207299977fe38a0055801c02ccc97db974
vikas <vikas2dx@gmail.com>
2023-07-04 00:11:59 +0530

    code optimize

--- videoloop.py
+++ videoloop.py
@@ -1,9 +1,10 @@
 import cv2
 widow_name="window"
+videoFileName="video.mp4"
 #Loop for playing video in repeat model
 while True:
-    isClosed=False
-    video=cv2.VideoCapture("video.mp4")
+    isClose=False
+    video=cv2.VideoCapture(videoFileName)
     #Set Video Frame to Full Screen
     cv2.namedWindow(widow_name, cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty(widow_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
@@ -14,12 +15,12 @@ while True:
            cv2.imshow(widow_name,frame)
            #Esc button(27) to exit the video
            if(cv2.waitKey(1)==27):
-               isClosed=True
+               isClose=True
                break
        else:
            break
     #Close Repeat Video Loop
-    if(isClosed):
+    if(isClose):
         break
 # Release video and destory frame after ESC press
 video.release()
